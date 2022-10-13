c702e905ed43fdf7a4bc86d1fe9504d5bc4d6d42
horacio albornoz <horaciomatiasalbornoz@gmail.com>
2022-10-13 15:00:21 -0300

    deloy

--- proyectoFinal/proyectoFinal/settings.py
+++ proyectoFinal/proyectoFinal/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cvs#tm%7j)d95v&bnn-m#$kfi$2ca29@fise_u(=9$ip^k-(g&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['espacio-bonhomia.herokuapp.com', '127.0.0.1']
 
 
